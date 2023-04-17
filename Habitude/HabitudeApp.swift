48a12b5991c0e83e62473ba916ca6f052b343d16
friddler <yhma22.nilsfr@folkuniversitetet.nu>
2023-04-17 11:20:02 +0200

    Initial Commit

---
+++ Habitude/HabitudeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitudeApp.swift
+//  Habitude
+//
+//  Created by Frida Nilsson on 2023-04-17.
+//
+
+import SwiftUI
+
+@main
+struct HabitudeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
